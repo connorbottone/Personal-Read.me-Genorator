5a5eae1d4b99b867cd1c8a786fd694c45ce48514
connorbottone <42557300+connorbottone@users.noreply.github.com>
2022-12-16 20:47:30 -0500

    reconfigured fucntion and debugged, Removd function to cycle badges and just added templ literal with liscense name

--- generateMarkDown.js
+++ generateMarkDown.js
@@ -1,20 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// const selected = `[![License: ${license}](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/${license}/)`
+// }// If there is no license, return an empty string
+// function renderLicenseBadge({license}) {
+  
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = ({projectTitle,description,install,usage,license,features,contributions,test,contactEmail,contactGit}) => 
+   ` #<${projectTitle}>
 
+  ## Description
+- ${description}
+  
+  ## Table of Contents
+  
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  ${install}
+  
+  ## Usage
+ ${usage}
+  
+     
+ ## License
+  
+  ${license}
+  
+  ---
+  
+  🏆 
+  
+  ## Badges
+  
+  - [![License: ${license}](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/${license}/)
+  
+  ## Features
+  
+ ${features}
+  
+  ## How to Contribute
+  
+${contributions}  
+  ## Tests
+  ${test}
+ 
+  ## Questions 
+Any questions regarding this application contact me at ${contactEmail}
+[Link to my Github profile](${contactGit})
 `;
-}
+
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,23 +1,69 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require('./generateMarkDown')
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What do you want the Title of Your Project to be ?',
+        },
 
-];
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining the what, why, and how of your project.',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data (err) => {
-        if (err) {
-             return console.log(err);
-             }
-console.log("created");
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Provide instructions and examples for use.',
+            choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'BSD 2-Clause License','Eclipse Public License 1.0','GNU GPL v3','IBM Public License Version 1.0','ISC License (ISC)','The MIT License']//fill in the liscense options here 
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'list the features of your project here',
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so here.',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: ' Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+        },
+        {
+            type: 'input',
+            name: 'contactEmail',
+            message: 'What is your email adress?',
+        },
+        {
+            type: 'input',
+            name: 'contactGit',
+            message: 'Copy a link to your Github here?',
+        },
+
+    ])
+
+    .then((answers) => {
+        const readMecontent = generateMarkdown(answers)
+        fs.writeFile('myREADME.MD', readMecontent, (err) => 
+        err ? console.log(err) : console.log( "File has been generated!"))
+
     });
-};
 
-// TODO: Create a function to initialize app
-function init() { }
-
-// Function call to initialize app
-init();
